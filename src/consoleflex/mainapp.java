1eb62b82112c495d8797f673593251205b60a389
georgemoralis <giorgosmrls@gmail.com>
2018-02-26 22:55:32 +0200

    Some work on mess/msdos.java for detecting system

--- src/consoleflex/mainapp.java
+++ src/consoleflex/mainapp.java
@@ -5,17 +5,24 @@
  */
 package consoleflex;
 
+import static WIP.arcadeflex.libc_v2.ConvertArguments;
+import static WIP.arcadeflex.libc_v2.argc;
+import static WIP.arcadeflex.libc_v2.argv;
+import old.arcadeflex.osdepend;
+
 /**
  *
  * @author shadow-laptop
  */
-public class main {
+public class mainapp {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConvertArguments("consoleflex", args);
+        args = null;
+        System.exit(osdepend.main(argc, argv));
     }
-    
+
 }
